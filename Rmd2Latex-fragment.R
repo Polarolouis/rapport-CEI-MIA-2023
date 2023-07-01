d886ad0d8ac763a5bbfb76a93297bcc384d30492
Polarolouis <louis.lacoste@hotmail.fr>
2023-07-01 18:09:48 +0200

    Adding a function to convert an Rmd to a single latex fragment

---
+++ Rmd2Latex-fragment.R
@@ -0,0 +1,7 @@
+require("knitr")
+create_latex <- function(f) {
+    knitr::knit(f, "/tmp/tmp-outputfile.md")
+    newname <- paste0(tools::file_path_sans_ext(f), ".tex")
+    mess <- paste("pandoc -f markdown -t latex -p -o", shQuote(newname), "/tmp/tmp-outputfile.md")
+    system(mess)
+}
